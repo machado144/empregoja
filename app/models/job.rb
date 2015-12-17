e2c34016c13f99208c438a101406497365c475c5
machado <murilo.paula.machado@gmail.com>
2015-12-17 21:57:43 -0200

    Add Feature: Recent jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,8 @@ class Job < ActiveRecord::Base
   belongs_to :category
 
   validates :title, :category, :description, :location, presence: true
+
+  def recent?
+    created_at > 5.days.ago
+  end
 end
