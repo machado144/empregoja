e09bd19007336bb052ff4eb16f5d09d7bf284c42
machado <murilo.paula.machado@gmail.com>
2015-12-17 20:57:03 -0200

    Add Feature: User create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,24 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      flash[:warning] = "Warning! All fields are mandatory."
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
