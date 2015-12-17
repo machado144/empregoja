17e3ba11c52f0ab3b73c1ce0cf337c20ff579b55
machado <murilo.paula.machado@gmail.com>
2015-12-17 21:20:23 -0200

    Add Feature: User edit companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update]
+
   def new
     @company = Company.new
   end
@@ -14,10 +16,24 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    @company = Company.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @company.update(company_params)
+      redirect_to @company
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
 
   def company_params
     params.require(:company).permit(:name, :location, :mail, :phone)
